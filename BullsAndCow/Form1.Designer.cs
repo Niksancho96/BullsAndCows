267e31dc22de9f0b1cffe1158240614159328b8e
Niko <niksancho96@gmail.com>
2014-02-17 20:37:24 +0200

    Upload
    New files

---
+++ BullsAndCow/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace BullsAndCow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.about = new System.Windows.Forms.Button();
+            this.newplayer = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // about
+            // 
+            this.about.Location = new System.Drawing.Point(103, 38);
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(75, 23);
+            this.about.TabIndex = 1;
+            this.about.Text = "За играта";
+            this.about.UseVisualStyleBackColor = true;
+            this.about.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // newplayer
+            // 
+            this.newplayer.Location = new System.Drawing.Point(12, 38);
+            this.newplayer.Name = "newplayer";
+            this.newplayer.Size = new System.Drawing.Size(85, 23);
+            this.newplayer.TabIndex = 2;
+            this.newplayer.Text = "Ново число";
+            this.newplayer.UseVisualStyleBackColor = true;
+            this.newplayer.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Провери";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 70);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(444, 22);
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(418, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 92);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.newplayer);
+            this.Controls.Add(this.about);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bulls and Cows v.0.1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button about;
+        private System.Windows.Forms.Button newplayer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ BullsAndCow/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BullsAndCow
+{
+    public partial class Form1 : Form
+    {
+        private int lenght = 4;
+        private int bulls, cows;
+
+        private string numbers = "123456789";
+        private string generated;
+        private string caption;
+        private string message;
+
+        public Form1()
+        {
+            InitializeComponent();
+            genNumber();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            toolStripStatusLabel2.Text = "Текущо генерирано число: " + this.generated;
+        }
+
+        private string genNumber()
+        {
+            Random rand = new Random();
+            int random = rand.Next(1, 5);
+            string shuffle = new string(this.numbers.ToCharArray().OrderBy(s => (rand.Next(2) % 2) == 0).ToArray());
+            this.generated = shuffle.Substring(0, this.lenght);
+            toolStripStatusLabel2.Text = "Текущо генерирано число: " + this.generated;
+            return this.generated;
+        }
+
+        private bool correctInput(string Input)
+        {
+            return (Input.Length < 4 || Input.Length == 0 || Input.Length > 4) ? true : false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.message = "Тази игра е разработена от Нико Митов! Версия 0.1 Alpha VoodooHeadsTV!";
+            this.caption = "За играта!";
+            MessageBox.Show(this.message, this.caption);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.message = "Компютъра генерира ново число!";
+            this.caption = "Ново число";
+            genNumber();
+            MessageBox.Show(this.message, this.caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string getPlayerInput = textBox1.Text.ToString();
+            if (correctInput(getPlayerInput) == true)
+            {
+                this.caption = "Грешен вход!";
+                this.message = "Данните които сте въвели, са невалидни!";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            this.bulls = 0;
+            this.cows = 0;
+
+            if (String.Compare(getPlayerInput, this.generated) == 0)
+            {
+                this.bulls = 4;
+                this.cows = 0;
+
+                this.caption = "Победа!";
+                this.message = "Вие познахте числото генерирано от компютъра!";
+                
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (getPlayerInput[0] == this.generated[0]) this.bulls++;
+                if (getPlayerInput[0] == this.generated[1] || getPlayerInput[0] == this.generated[2] || getPlayerInput[0] == this.generated[3]) this.cows++;
+                if (getPlayerInput[1] == this.generated[1]) this.bulls++;
+                if (getPlayerInput[1] == this.generated[0] || getPlayerInput[1] == this.generated[2] || getPlayerInput[1] == this.generated[3]) this.cows++;
+                if (getPlayerInput[2] == this.generated[2]) this.bulls++;
+                if (getPlayerInput[2] == this.generated[1] || getPlayerInput[2] == this.generated[0] || getPlayerInput[2] == this.generated[3]) this.cows++;
+                if (getPlayerInput[3] == this.generated[3]) this.bulls++;
+                if (getPlayerInput[3] == this.generated[1] || getPlayerInput[3] == this.generated[2] || getPlayerInput[3] == this.generated[0]) this.cows++;
+
+                this.caption = "Резултат!";
+                this.message = "Вие имате " + this.bulls + " бикове и " + this.cows + " крави!";
+                MessageBox.Show(this.message, this.caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
